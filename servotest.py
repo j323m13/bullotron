0362fff245fe1bac739642beef5ee3a1b4d359fa
MajorTwip <majortwip@comstock.ch>
2021-10-23 13:00:43 +0200

    modified servotest to work with active_low-PWM

--- servotest.py
+++ servotest.py
@@ -1,23 +1,26 @@
 import pindefinitions as pin
 
-from gpiozero import Servo
+from gpiozero import PWMOutputDevice 
+##from gpiozero import Servo #Does not support active_high=False
 from time import sleep
+
+
 
 #from gpiozero.pins.pigpio import PiGPIOFactory
 #factory = PiGPIOFactory()
 #servo = Servo(pin.SERVO, pin_factory=factory)
-servo = Servo(pin.SERVO)
+servo = PWMOutputDevice(pin.SERVO,active_high=False)
 
 while True:
-    servo.mid()
+    servo.value =0.01
     print("mid")
     sleep(0.5)
-    servo.min()
+    servo.value = 0.02
     print("min")
     sleep(1)
-    servo.mid()
+    servo.value = 0.01
     print("mid")
     sleep(0.5)
-    servo.max()
+    servo.value = 0.2
     print("max")
     sleep(1)

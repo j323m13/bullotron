a116d0a9491b7eecfb1e91bc725af42357199f95
MajorTwip <majortwip@comstock.ch>
2021-11-24 10:38:52 +0100

    initialised States

--- hw_service.py
+++ hw_service.py
@@ -31,15 +31,20 @@ __status__ = 'Draft'
 
 class BullotronHW(StateMachine):
     init_hw = State('InitHW', initial=True)
-    opening_full = State('Fully opening')
-    
-    start = on_debt.to(under_approval)
-    pause = under_approval.to(on_debt)
-    finish = under_approval.to(approved)
-    
-    reset = approved.to(on_debt) | under_approval.to(on_debt)
-    next_step = on_debt.to(under_approval) | under_approval.to(on_debt)
-    
+    sens_fill = State('Sensing Level')
+    closed = State('Lid closing')
+    open_to_blow = State('Lid opening for blowing')
+    blowing = State('Blowing')
+    open_full = State('Fully opening')
+    open_setting =State('Opening to dynamic angle') 
+
+    init = init_hw.to(closed)
+    close = sens_fill.to(closed) | open_to_blow.to(closed) | open_full.to(closed) | open_setting.to(closed) 
+    sense = close.to(sens_fill)
+    blow_cycle = closed.to(open_to_blow) | open_to_blow.to(blowing) | blowing.to(closed)
+    fill = closed.to(open_full)
+    set_angle = closed.to(open_setting)
+
     def on_on_debt(self):
         print('Document on hold.')
 

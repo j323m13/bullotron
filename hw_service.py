b64207c155418e8dcbceefb2e2417e53b25784a0
MajorTwip <majortwip@comstock.ch>
2021-11-24 11:29:21 +0100

    implemented basic FSM sceleton

--- hw_service.py
+++ hw_service.py
@@ -9,6 +9,9 @@ This Code is licenced under the Apache License 2.0
 """
 
 # Built-in/Generic Imports
+import logging
+from argparse import ArgumentParser
+from time import sleep
 import os
 import sys
 # […]
@@ -45,8 +48,66 @@ class BullotronHW(StateMachine):
     fill = closed.to(open_full)
     set_angle = closed.to(open_setting)
 
-    def on_on_debt(self):
-        print('Document on hold.')
+    def on_init(self):
+        logging.info("Initialize Hardware")
+        logging.debug("shut off fan")
+        logging.debug("close lid")
 
-        
-bullotron = BullotronHW()
+    def on_enter_closed(self):
+        logging.info("closed")
+        logging.debug("enable RC-receiver")
+        logging.debug("waiting for: RC, Buttonpress for start, order to fill, setup")
+        sleep(4)
+        self.blow_cycle()
+
+    def on_exit_closed(self):
+        logging.debug("disable RC-receiver")
+
+
+    def on_close(self):
+        logging.info("Closing")
+        logging.debug("shut off fan")
+        logging.debug("close lid")
+
+    def on_enter_open_to_blow(self):
+        logging.info("Opening to blow")
+        logging.debug("Set lid to")
+        sleep(2)
+        logging.debug("trigger blow")
+        self.blow_cycle()
+
+    def on_enter_blowing(self):
+        logging.info("Blowing")
+        logging.debug("Set fan to")
+        logging.debug("Wait for")
+        sleep(2)
+        self.blow_cycle()
+
+
+    def on_exit_blowing(self):
+        logging.debug("stop fan")
+
+
+
+def main():
+    # set loglevel from CLI
+    parser = ArgumentParser()
+    parser.add_argument("-d", "--debug", help="Increase output verbosity", action="store_const", const=logging.DEBUG, default=logging.INFO)
+    args = parser.parse_args()
+    logging.basicConfig(level=args.debug)
+    logging.debug("DEBUG set")
+
+    logging.info("Setting up hardware modes")
+    #TODO
+
+    logging.info("connecting to redis")
+    #TODO
+
+    logging.info("Initialize the FSM")
+    bullotron = BullotronHW()
+    bullotron.init()
+
+
+if __name__ == '__main__':
+    main()
+

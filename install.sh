c780d1b14f5654c6a08308e9ffd9529cbe0eb77b
MajorTwip <majortwip@comstock.ch>
2021-11-24 14:55:58 +0100

    Added services

--- install.sh
+++ install.sh
@@ -10,3 +10,13 @@ sudo systemctl enable redis-server
 
 #install python libs
 pip3 install -r requirements.txt
+
+mkdir /opt/bullotron
+cp ./*.py /opt/bullotron/
+
+cp ./*.service /etc/systemd/system/
+sudo systemctl daemon-reload
+
+sudo systemctl enable redis-server && sudo systemctl start redis-server
+sudo systemctl enable bullotron-hw && sudo systemctl start bullotron-hw
+sudo systemctl enable bullotron-display && sudo systemctl start bullotron-display

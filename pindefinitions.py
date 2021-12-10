b9890e794a27c44d1a6f7a15ef06c2db34287ebf
j323m13 <jeremie.equey@students.ffhs.ch>
2021-12-10 22:52:27 +0100

    comments

--- pindefinitions.py
+++ pindefinitions.py
@@ -1,12 +1,20 @@
+
+#Buttons
 SW1 = 21
 SW2 = 20
 SW3 = 16
+
+#in and out
 IN1 = 19
 IN2 = 26
 OUT1 = 5
 OUT2 = 6
 OUT3 = 13
+
+#servos
 SERVO = 27
+
+# GPI for lcd
 LCD_RS = 4
 LCD_E = 17
 LCD_D4 = 18 

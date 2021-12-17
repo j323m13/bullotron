4e6605dad4c06f8e6ddde8f4a6bb22f6334d01f2
MajorTwip <majortwip@comstock.ch>
2021-12-17 11:05:17 +0100

    wrapped notify in "def"

--- notification.py
+++ notification.py
@@ -1,9 +1,10 @@
 import http.client, urllib
-conn = http.client.HTTPSConnection("api.pushover.net:443")
-conn.request("POST", "/1/messages.json",
-  urllib.parse.urlencode({
-    "token": "APP_TOKEN",
-    "user": "USER_KEY",
-    "message": "Bullotrons Soap Level is low",
-  }), { "Content-type": "application/x-www-form-urlencoded" })
-conn.getresponse()
+def notify():
+  conn = http.client.HTTPSConnection("api.pushover.net:443")
+  conn.request("POST", "/1/messages.json",
+    urllib.parse.urlencode({
+      "token": "APP_TOKEN",
+      "user": "USER_KEY",
+      "message": "Bullotrons Soap Level is low",
+    }), { "Content-type": "application/x-www-form-urlencoded" })
+  conn.getresponse()
